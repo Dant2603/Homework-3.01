86f857af37c8cc5755ebe8c8b0453e6a5e694741
Daniil Maiboroda <Dant2603@gmail.com>
2024-10-21 17:22:22 +0300

    Initial Commit

---
+++ Homework 3.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3.01
+//
+//  Created by Мария Гетманская on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Homework 3.01Tests/Homework_3_01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_3_01Tests.swift
+//  Homework 3.01Tests
+//
+//  Created by Мария Гетманская on 21.10.2024.
+//
+
+import Testing
+@testable import Homework_3_01
+
+struct Homework_3_01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
